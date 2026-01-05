b320105d072f678384fd0025ce571a4153091002
giganotosorus mort <giganotosorus.mort@gmail.com>
2026-01-05 21:24:30 +0100

    Changement de l'adresse css et ajout des titres de l'accueil

--- index.php
+++ index.php
@@ -4,13 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
     <?php include("components/header.php"); ?>
     
     <div class="container">
-
+        
+    <div>
+        <h1>Découvrez les projets étudiants</h1>
+        <h3>Votez pour les meilleures idées !</h3>
+    </div>
+        
     </div>
 
     <?php include("components/footer.php"); ?>
