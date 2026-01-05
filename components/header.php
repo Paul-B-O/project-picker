3f80074254856a9a118d3cc0b8dc31874a91f5f7
giganotosorus mort <giganotosorus.mort@gmail.com>
2026-01-05 21:24:09 +0100

    Ajout de la nav dans le header

--- components/header.php
+++ components/header.php
@@ -1,3 +1,21 @@
 <header>
-    
+    <div>
+        <img src="./assets/images/logo.svg" alt="">
+    </div>
+    <nav>
+        <ul>
+            <li>
+                <a href="index.php">Accueil</a>
+            </li>
+            <li>
+                <a href="">Proposer un projet</a>
+            </li>
+            <li>
+                <a href="">Mes projets</a>
+            </li>
+            <li>
+                <a href="">Connexion</a>
+            </li>
+        </ul>
+    </nav>
 </header>
