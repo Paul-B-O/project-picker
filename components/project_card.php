d42ac06ab0be4d77667938ab0cb72153775614fe
giganotosorus mort <giganotosorus.mort@gmail.com>
2026-01-07 17:29:23 +0100

    Création du fichier et du html pour la card des projets

---
+++ components/project_card.php
@@ -0,0 +1,13 @@
+<article class="card">
+    <h3 class="card-title" >Application de gestion de notes</h3>
+    <div class="card-text">
+        <div>Pierre Dupont</div>
+        <div>15 novembre 2025</div>
+    </div>
+    <p>Une application web permettant aux étudiants de suivre leurs notes et moyennes par matière</p>
+
+    <div class="buttons">
+        <div class="badge badge-vote">28 votes</div>
+        <button class="btn btn-primary">Voir détails</button>
+    </div>
+</article>
